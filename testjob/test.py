82b73de107939897da00a1494cfafe6135bfd60f
hwan <ckdghks87@snu.ac.kr>
2021-07-30 22:00:52 +0900

    spark.py 변경

--- testjob/test.py
+++ testjob/test.py
@@ -7,12 +7,24 @@ import mysql.connector
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from dependencies.spark import start_spark
+from jobs.with_geopandas import geopandas_df_to_spark_for_points
 
-test_spark, *_ = start_spark()
+def download_file(url):
+    filename = url.split('/')[-1]
+    response = urllib.request.urlopen(url)
+    content = response.read()
+
+spark, *_ = start_spark()
 # test_spark = test_spark.read.csv('../train.csv', header=True, inferSchema=True).select(['SUBWAY_ID', 'STATN_ID', 'STATN_NM'])
 
-test_df = test_spark.read.format("jdbc").option("url", "jdbc:mysql://localhost:3306/test_db") \
+# Spark with GeoPandas
+sdf = geopandas_df_to_spark_for_points(spark, gdf)
+display(sdf)
+# Spark with MySQL
+'''
+test_df = spark.read.format("jdbc").option("url", "jdbc:mysql://localhost:3306/test_db") \
     .option("driver", "com.mysql.jdbc.Driver").option('dbtable', "Trains") \
     .option("user", "root").option("password", "9315").load()
 
 test_df.show()
+'''
