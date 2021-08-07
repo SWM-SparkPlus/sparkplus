6f69a90e7f54247696ec69eaf5faddc30b1245a6
chanwooleeme <upup1810@gmail.com>
2021-08-08 06:41:56 +0900

    Convert mysql to spark df(All tables)

--- package/example.py
+++ package/example.py
@@ -22,11 +22,78 @@ import mysql.connector
 # result = gis.coord_to_dong(spark, gdf, myDf, "X", "Y")
 # result.show()
 import sys
+
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+
+driver = "com.mysql.cj.jdbc.Driver"
+url = "jdbc:mysql://host.docker.internal:3306/sparkplus"
+user = "root"
+password = "sparkplus"
+
 spark = SparkSession.builder.appName("hello").getOrCreate()
-
-test_df = spark.read.format("jdbc").option("url", "jdbc:mysql://host.docker.internal:3306/sparkplus") \
-    .option("driver", "com.mysql.cj.jdbc.Driver").option('dbtable', "jibun_address_daejeon") \
-    .option("user", "root").option("password", "sparkplus").load()
-
-test_df.show()
+li = [
+	'additional_info_busan',
+	'additional_info_chungbuk',
+	'additional_info_chungnam',
+	'additional_info_daegu',
+	'additional_info_daejeon',
+	'additional_info_gangwon',
+	'additional_info_gwangju',
+	'additional_info_gyeongbuk',
+	'additional_info_gyeonggi',
+	'additional_info_gyeongnam',
+	'additional_info_incheon',
+	'additional_info_jeju',
+	'additional_info_jeonbuk',
+	'additional_info_jeonnam',
+	'additional_info_sejong',
+	'additional_info_seoul',
+	'additional_info_ulsan',
+	'jibun_address_busan',
+	'jibun_address_chungbuk',
+	'jibun_address_chungnam',
+	'jibun_address_daegu',
+	'jibun_address_daejeon',
+	'jibun_address_gangwon',
+	'jibun_address_gwangju',
+	'jibun_address_gyeongbuk',
+	'jibun_address_gyeonggi',
+	'jibun_address_gyeongnam',
+	'jibun_address_incheon',
+	'jibun_address_jeju',
+	'jibun_address_jeonbuk',
+	'jibun_address_jeonnam',
+	'jibun_address_sejong',
+	'jibun_address_seoul',
+	'jibun_address_ulsan',
+	'roadname_address_busan',
+	'roadname_address_chungbuk',
+	'roadname_address_chungnam',
+	'roadname_address_daegu',
+	'roadname_address_daejeon',
+	'roadname_address_gangwon',
+	'roadname_address_gwangju',
+	'roadname_address_gyeongbuk',
+	'roadname_address_gyeonggi',
+	'roadname_address_gyeongnam',
+	'roadname_address_incheon',
+	'roadname_address_jeju',
+	'roadname_address_jeonbuk',
+	'roadname_address_jeonnam',
+	'roadname_address_sejong',
+	'roadname_address_seoul',
+	'roadname_address_ulsan',
+	'roadname_code'
+]
+table_df_list = list()
+for table_name in li:
+    name = table_name + "_df"
+    name = spark.read.format("jdbc")\
+        .option("driver", "com.mysql.cj.jdbc.Driver")\
+        .option("url", url) \
+        .option("dbtable", table_name) \
+        .option("user", user)\
+        .option("password", password)\
+        .load()
+    table_df_list.append(name)
+table_df_list[2].show()
