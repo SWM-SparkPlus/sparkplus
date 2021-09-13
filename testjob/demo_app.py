f7424f50e30e0806e493ce74f0f0ab9f1d900adb
hwan <ckdghks87@snu.ac.kr>
2021-09-14 08:02:50 +0900

    demo_app.py for emr

--- testjob/demo_app.py
+++ testjob/demo_app.py
@@ -1,8 +1,12 @@
 import os
 import sys
 
+from shapely.geometry import Polygon
+import folium
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import encode
+import geopandas as gpd
+import h3
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
@@ -15,8 +19,8 @@ url = "jdbc:mysql://localhost:3306/sparkplus"
 user = "sparkplus"
 password = "sparkplus"
 
-filepath = "../resource/data/daegu_streetlight.csv"
-shp = "../resource/EMD_202101/TL_SCCO_EMD.shp"
+filepath = "/home/hadoop/spark-plugin/resource/data/daegu_streetlight.csv"
+shp = "/home/hadoop/spark-plugin/resource/EMD_202101/TL_SCCO_EMD.shp"
 
 if __name__ == "__main__":
 
@@ -28,7 +32,7 @@ if __name__ == "__main__":
     gdf = gdf.to_crs(4326)
 
     dataFrameReader = session.read
-
+    
     my_sdf = dataFrameReader \
         .option("header", True) \
         .format("csv") \
@@ -39,3 +43,20 @@ if __name__ == "__main__":
 
     h3_df = join_with_h3(my_sdf, "경도", "위도", 10)
     h3_df.show()
+
+    """
+    def to_polygon(l):
+	    return Polygon(h3.h3_to_geo_boundary(l, geo_json=True))
+
+    temp = [35.8734, 128.6103]
+
+    gdf_h3 = h3_df.toPandas()
+    gdf_h3 = gpd.GeoDataFrame(gdf_h3)
+    gdf_h3['geometry'] = gdf_h3['h3'].apply(to_polygon)
+    gdf_h3.crs = {'init': 'epsg:4326'}
+
+    m =folium.Map(temp, zoom_start=14)
+    folium.GeoJson(gdf_h3).add_to(m)
+
+    m.save('daegu1.html')
+    """
