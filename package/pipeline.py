6f2fa3ae0950f1782b154b4b9f21a2b19bf266e8
chanwooleeme <upup1810@gmail.com>
2021-09-18 20:40:46 +0000

    Polyfill 코드 추가

--- package/pipeline.py
+++ package/pipeline.py
@@ -9,17 +9,18 @@ import mysql.connector
 import sys
 import os
 import gis
+import h3pandas
 
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 spark = SparkSession.builder.appName("Spark App").getOrCreate()
-dict = gis.load_table(spark) #table dictionary 불러오기
-jibun_dict = {}
-for key, val in list(dict.items()) :
-	if 'jibun_address' in key :
-		result = dict[key].select(['bupjungdong_code', 'sido', 'sigungu', 'bupjungeupmyeondong']).dropDuplicates(['bupjungdong_code']).orderBy("bupjungdong_code")
-		jibun_dict[key] = result
-		del dict[key]
-print(jibun_dict)
-# gdf = gis.load_shp(spark, "../resource/TL_SCCO_EMD.shp") #법정동 shp 파일 불러오기
-# sdf_df = gis.gdf_to_spark_wkt(spark, gdf) #spark에서 읽을 수 있도록 wkt로 변환
+# dict = gis.load_table(spark) #table dictionary 불러오기
+# jibun_dict = {}
+# for key, val in list(dict.items()) :
+# 	if 'jibun_address' in key :
+# 		result = dict[key].select(['bupjungdong_code', 'sido', 'sigungu', 'bupjungeupmyeondong']).dropDuplicates(['bupjungdong_code']).orderBy("bupjungdong_code")
+# 		jibun_dict[key] = result
+gdf = gis.load_shp(spark, "../resource/EMD_202101/TL_SCCO_EMD.shp") #법정동 shp 파일 불러오기
+sdf_df = gis.gdf_to_spark_wkt(spark, gdf) #spark에서 읽을 수 있도록 wkt로 변환
+gdf_h3 = gdf.h3.polyfill(15)
+print(gdf)
