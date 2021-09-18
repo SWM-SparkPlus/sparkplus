6b34ea39fffdc394b325a412c2c11d586343fd1e
chanwooleeme <upup1810@gmail.com>
2021-09-18 19:26:11 +0000

    법정동 코드 DF 생성 및 중복제거

--- package/pipeline.py
+++ package/pipeline.py
@@ -13,6 +13,13 @@ import gis
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 spark = SparkSession.builder.appName("Spark App").getOrCreate()
-dic = gis.load_table(spark) #table dictionary 불러오기
-gdf = gis.load_shp(spark, "../resource/TL_SCCO_EMD.shp") #법정동 shp 파일 불러오기
-sdf_df = gis.gdf_to_spark_wkt(spark, gdf) #spark에서 읽을 수 있도록 wkt로 변환
+dict = gis.load_table(spark) #table dictionary 불러오기
+jibun_dict = {}
+for key, val in list(dict.items()) :
+	if 'jibun_address' in key :
+		result = dict[key].select(['bupjungdong_code', 'sido', 'sigungu', 'bupjungeupmyeondong']).dropDuplicates(['bupjungdong_code']).orderBy("bupjungdong_code")
+		jibun_dict[key] = result
+		del dict[key]
+print(jibun_dict)
+# gdf = gis.load_shp(spark, "../resource/TL_SCCO_EMD.shp") #법정동 shp 파일 불러오기
+# sdf_df = gis.gdf_to_spark_wkt(spark, gdf) #spark에서 읽을 수 있도록 wkt로 변환
