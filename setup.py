c49532a5baeba03ea2b9dd8d3f431aca5400004e
hwan <ckdghks87@snu.ac.kr>
2021-09-24 14:20:55 +0900

    setting for pipy

--- setup.py
+++ setup.py
@@ -1,4 +1,37 @@
-import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='sparkplus-test',
+    version="0.1",
+    description='test package for sparkplus',
+    author='sparkplus',
+    author_email='meadea27@gmail.com',
+    url='https://github.com/SWM-SparkPlus/spark-plugin',
+    license='MIT',
+    py_modules=['conversion', 'load_database'],
+    python_requires='>=3',
+    install_requires=[
+        'numpy', 
+        'pandas', 
+        'geopandas', 
+        'geospark', 
+        'h3', 
+        'geopy', 
+        'pyarrow', 
+        'rtree', 
+        'shapely'],
+    package=find_packages(exlcude=[]),
+    keywords=['spark', 'geo'],
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
 
 setuptools.setup(
     name="spark-plus",
