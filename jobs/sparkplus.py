5466e5210f9f5427612787a9158ecd9385b50c16
hwan <ckdghks87@snu.ac.kr>
2021-09-29 16:31:56 +0900

    CustomDataFrame.coord_to_pnu

--- jobs/sparkplus.py
+++ jobs/sparkplus.py
@@ -1,24 +1,52 @@
-from jobs.conversion import join_with_table
-from pyspark.sql.functions import lit, udf
-from pyspark.sql import Dataframe
+import os
+import sys
+
+from pyspark.sql.functions import lit, udf, pandas_udf
+from pyspark.sql import DataFrame
+from pyspark.sql.types import StringType
+
+import geopandas as gpd
 import h3
 
-class CustomDataFrame(Dataframe):
+class CustomDataFrame(DataFrame):
 
 	def __init__(self, origin_df):
-		self.origin_df = origin_df
-
+		self._origin_df = origin_df
+		# self._gdf = gdf
+	
 	def coord_to_h3(self, x_colname, y_colname, h3_level):
 		udf_to_h3 = udf(
-        	lambda x, y: h3.geo_to_h3(float(x), float(y), h3_level), returnType=StringType()
-    	)
-		res_h3 = self.origin_df.withColumn("h3", udf_to_h3(self.origin_df[y_colname], self.origin_df[x_colname]))
+			lambda x, y: h3.geo_to_h3(float(x), float(y), h3_level), returnType=StringType()
+		)
+		res_h3 = self._origin_df.withColumn("h3", udf_to_h3(self._origin_df[y_colname], self._origin_df[x_colname]))
 		return res_h3
+
+	def create_sjoin_pnu(self,  gdf, join_column_name):
+		def sjoin_settlement(x, y):
+			gdf_temp = gpd.GeoDataFrame(
+				data=[[x] for x in range(len(x))], geometry=gpd.points_from_xy(x, y)
+			).set_crs(epsg=4326, inplace=True)
+			settlement = gpd.sjoin(gdf_temp, gdf, how='left', op='within')
+			settlement = settlement.drop_duplicates(subset='geometry')
+
+			return (
+				settlement.agg({"PNU": lambda x: str(x)})
+				.reset_index()
+				.loc[:, join_column_name]
+				.astype("str")
+			)
+		return pandas_udf(sjoin_settlement, returnType=StringType())
+
+	def coord_to_pnu(self, gdf, x_colname, y_colname):
+		sjoin_udf = self.create_sjoin_pnu(gdf, "PNU")
+		res_df = self._origin_df.withColumn("PNU", sjoin_udf(self._origin_df[x_colname], self._origin_df[y_colname]))
+		
+		return res_df
 
 	def coord_to_zip(self):
 		self.origin_df = self
 
-	def coord_to_emd(self):
+	def coord_to_emd(self, x_colname, y_colname):
 		self.origin_df = self
 
 	def coord_to_address(self):
---
+++ sparkplus/testjob/test_cls.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+import geopandas as gpd
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+
+from jobs.sparkplus import CustomDataFrame
+from pyspark.sql import SparkSession
+
+
+driver = "com.mysql.cj.jdbc.Driver"
+url = "jdbc:mysql://localhost:3306/sparkplus"
+user = "sparkplus"
+password = "sparkplus"
+
+shp_path = "../resource/shp/LSMD_CONT_LDREG_27_202109.shp"
+data_path = "../resource/data/daegu_streetlight.csv"
+
+session = (
+        SparkSession.builder.appName("demo_app")
+        .config(
+            "spark.driver.extraClassPath",
+            "/usr/lib/spark/jars/mysql-connector-java-8.0.26.jar",
+        )
+        .getOrCreate()
+    )
+
+dataFrameReader = session.read
+
+gdf = gpd.read_file(shp_path, encoding='euc-kr')
+gdf.crs = "epsg:5174"
+gdf = gdf.to_crs(epsg=4326)
+
+my_sdf = (
+    dataFrameReader.option("header", True)
+        .format("csv")
+        .load(data_path, encoding="euc-kr")
+)
+
+df = CustomDataFrame(my_sdf)
+
+res_df = df.coord_to_pnu(gdf, '경도', '위도')
+
+res_df.show()
