537b1790e5a5326397823df9bc5461836feae5e7
chanwooleeme <upup1810@gmail.com>
2021-10-06 19:35:03 +0000

    Create py_log.py

---
+++ jobs/py_log.py
@@ -0,0 +1,23 @@
+import logging
+import logging.handlers
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('[%(asctime)s[%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s')
+
+stremaHandler = logging.StreamHandler()
+fileHandler = logging.FileHandler('./output.logs')
+logger.setLevel(level=logging.DEBUG)
+
+stremaHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+
+logger.addHandler(stremaHandler)
+logger.addHandler(fileHandler)
+
+
+logger.setLevel(level=logging.DEBUG)
+logging.debug('DEBUG log')
+logging.info('INFO log')
+logging.warning('WARN log')
+logging.error('ERROR log')
+logging.critical('CRITICAL log')
