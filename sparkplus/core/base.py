542dadf1f66eddbb4c9dfc4a71c118f8186c6565
Tay Park <interrupt@kakao.com>
2021-10-10 02:58:10 +0900

    edit: Class init and docs

--- sparkplus/core/base.py
+++ sparkplus/core/base.py
@@ -1,5 +1,5 @@
 from geopandas.geodataframe import GeoDataFrame
-from pyspark.sql.functions import lit, udf, pandas_udf
+from pyspark.sql.functions import col, column, lit, udf, pandas_udf
 from pyspark.sql import DataFrame
 from pyspark.sql import types
 
@@ -7,67 +7,46 @@ import geopandas as gpd
 import h3
 
 
-class CustomDataFrame(object):
+class SPDataFrame(object):
     """
-    CustomDataFrame()
-    :param	origin_df
-    :param	gdf
-    :param	table_df
-    :param	x_colname
-    :param	y_colname
+    요약
+    -------
+    `SPDataFrame` 은 Spark DataFrame를 확장하며, 한국 주소체계를 더 쉽게 다룰 수 있도록 다양한 기능을 제공합니다.
 
-    coord_to_h3()
-    :param	h3_level
-
-    coord_to_pnu()
-
-    join_with_table()
+    사용법
+    -----
+    >>> import sparkplus
+    >>> df = spark.read...
+    >>> spdf = sparkplus.SPDataFrame(df) # Spark DataFrame 을 파라미터로 받아 사용합니다.
     """
 
     def __init__(self, dataFrame: DataFrame):
         self._df = dataFrame
 
-        # self.pnu_df = _coord_to_pnu(dataFrame, gdf, x_colname, y_colname).cache()
-        # self.joined_df = _join_with_table(table_df, self.pnu_df).cache()
-
-        # self.pnu_df.show()
-        # self._integrated_df = self.join_with_table()
-        # self._integrated_df.show()
-        # self._gdf = gdf
-
-    # def sjoin_settlement(self, lat, lng):
-    #         gdf_temp = gpd.GeoDataFrame(
-    #             data=[[x] for x in range(len(x))], geometry=gpd.points_from_xy(x, y)
-    #         ).set_crs(epsg=4326, inplace=True)
-    #         settlement: GeoDataFrame = gpd.sjoin(gdf_temp, gdf, how="left", op="within")
-    #         settlement = settlement.drop_duplicates(subset="geometry")
-
-    #         return settlement.agg({"PNU": str(x)})\
-    #             .reset_index()\
-    #             .loc[:, join_column_name]\
-    #             .astype("str")
-
-    # def create_sjoin_pnu(self, gdf: GeoDataFrame, join_column_name: str):
-    #     return pandas_udf(self.sjoin_settlement, returnType=types.StringType())
-
-    # def _coord_to_pnu(
-    #     origin_df: DataFrame, gdf: GeoDataFrame, x_colname: str, y_colname: str
-    # ):
-    #     sjoin_udf = create_sjoin_pnu(gdf, "PNU")
-    #     res_df = origin_df.withColumn(
-    #         "PNU", sjoin_udf(origin_df[x_colname], origin_df[y_colname])
-    #     )
-    #     return res_df
-
-    def _join_with_table(
-        self, table_df: DataFrame, pnu_df: GeoDataFrame, drop_dups: bool = True
+    def table_pnu_df_join(
+        self,
+        table_df: DataFrame,
+        pnu_df: GeoDataFrame,
+        drop_dup_bupjungdong_code: bool = True,
     ):
+        """
+        Args:
+            table_df (DataFrame): 데이터베이스로부터 생성한 Spark DataFrame
+            pnu_df (GeoDataFrame): shp Parquet로부터 생성한 GeoDataFrame
+            drop_dups (bool, optional): 법정동코드 중복 제거 여부. Defaults to True.
+
+        Raises:
+            `ValueError`
+
+        Returns:
+            `pyspark.sql.DataFrame`
+        """
         # 테이블 및 파케이 데이터프레임이 메모리에 상주하지 않다면 에러 발생
-        if table_df is None or pnu_df is None:
-            raise NotImplementedError
+        if not isinstance(table_df, DataFrame) or not isinstance(pnu_df, GeoDataFrame):
+            raise ValueError
 
         # 데이터베이스에 법정동 코드가 중복되어 중복 제거
-        if drop_dups:
+        if drop_dup_bupjungdong_code:
             table_df = table_df.dropDuplicates(["bupjungdong_code"])
 
         return pnu_df.join(
@@ -89,42 +68,42 @@ class CustomDataFrame(object):
     # def coord_to_pnu(self):
     #     return self.pnu_df
 
-    def coord_to_zipcode(self):
-        joined_df = self.joined_df.select("PNU", "zipcode")
-        res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
-        return res_df
+    # def coord_to_zipcode(self):
+    #     joined_df = self.joined_df.select("PNU", "zipcode")
+    #     res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
+    #     return res_df
 
-    def coord_to_emd(self):
-        joined_df = self.joined_df.select("PNU", "bupjungdong_code")
-        res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
-        return res_df
+    # def coord_to_emd(self):
+    #     joined_df = self.joined_df.select("PNU", "bupjungdong_code")
+    #     res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
+    #     return res_df
 
-    def coord_to_doromyoung(self):
-        joined_df = self.joined_df.select(
-            "PNU",
-            "sido",
-            "sigungu",
-            "roadname",
-            "is_basement",
-            "building_primary_number",
-            "building_secondary_number",
-            "bupjungdong_code",
-        )
-        res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
-        return res_df
+    # def coord_to_doromyoung(self):
+    #     joined_df = self.joined_df.select(
+    #         "PNU",
+    #         "sido",
+    #         "sigungu",
+    #         "roadname",
+    #         "is_basement",
+    #         "building_primary_number",
+    #         "building_secondary_number",
+    #         "bupjungdong_code",
+    #     )
+    #     res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
+    #     return res_df
 
-    def coord_to_jibun(self):
-        joined_df = self.joined_df.select(
-            "PNU",
-            "sido",
-            "sigungu",
-            "eupmyeondong",
-            "bupjungli",
-            "jibun_primary_number",
-            "jibun_secondary_number",
-        )
-        res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
-        return res_df
+    # def coord_to_jibun(self):
+    #     joined_df = self.joined_df.select(
+    #         "PNU",
+    #         "sido",
+    #         "sigungu",
+    #         "eupmyeondong",
+    #         "bupjungli",
+    #         "jibun_primary_number",
+    #         "jibun_secondary_number",
+    #     )
+    #     res_df = self.pnu_df.join(joined_df, "PNU", "leftouter")
+    #     return res_df
 
     """
 	def coord_to_zipcode2(self):
