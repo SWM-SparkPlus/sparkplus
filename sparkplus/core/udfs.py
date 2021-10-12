2c92a707eb9e16ec6bb5f6f8d708c380f968bf63
chanwooleeme <upup1810@gmail.com>
2021-10-12 21:04:06 +0000

    Create udfs

---
+++ sparkplus/core/udfs.py
@@ -0,0 +1,20 @@
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType, StringType, ArrayType
+
+sido_short_list = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"]
+sido_long_list = ["서울특별시","부산광역시","대구광역시","인천광역시","광주광역시","대전광역시","울산광역시","세종특별자치시",'경기도',"강원도","충청북도","충청남도","전라북도","전라남도","경상북도","경상남도","제주특별자치도"]
+sido_dictionary = dict(zip(sido_short_list, sido_long_list))
+sido_reverse_dictionary = dict(zip(sido_short_list, sido_long_list))
+
+@udf(IntegerType())
+def where_is_sido(split):
+	for i in range(len(split)):
+		if sido_dictionary.get(split[i]) or sido_reverse_dictionary.get(split[i]):
+			return i
+	return -1
+
+@udf(ArrayType(StringType()))
+def cleanse_split(idx, split):
+	if idx != -1:
+		return split[idx:]
+	return split
